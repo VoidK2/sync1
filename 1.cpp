d00b0c7fb45cd56ba15120db2523e0dc515a7d94
voidk2 <1399477504@qq.com>
2018-03-12 21:39:56 +0800

    2017lanqiaoB3

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout<<1<<endl;
+    long long a[30][30],ans;
+    memset(a,0,sizeof(a));
+    freopen("input.txt", "r", stdin);
+    for(int i=1; i<=29; i++) {
+        for(int j=1; j<=i; j++) {
+            scanf("%lld",&a[i][j]);
+        }
+    }
+    for(int i=2;i<=29;i++){
+        for(int j=1;j<=29;j++){
+            a[i][j]=a[i-1][j]/2+a[i-1][j-1]/2;
+        }
+    }
+    for(int i=1;i<=29;i++){
+        ans=max(ans,a[29][i]);
+    }
+    printf("max = %lld\n",ans);
     return 0;
 }
