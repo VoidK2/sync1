0ac9c6b7a411e72a3a8b4107741bd6be839a0ff6
voidk2 <1399477504@qq.com>
2018-03-15 21:49:14 +0800

    init 3.cpp

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool chage(bool i){
+    if(i==true)
+    return false;
+    else
+    return true;
+
+}
+int main() {
+    freopen("input.txt","r",stdin);
+    int m,n;
+    scanf("%d%d",&n,&m);
+    bool a[1000][1000];
+    memset(a,true,sizeof(a));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            a[i][j]=
+        }
+    }
+}
---
+++ 3.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define 0 white
+#define 1 black
+char dir[3]= {'U','R','D','L'};
+int diri=0;
+int main() {
+    freopen("input.txt","r",stdin);
+    bool a[110][110];
+    memset(a,true,sizeof(a));
+    int n,m,x,y,k;
+    char s;
+
+
+    scanf("%d%d",&n,&m);
+    for(int i=1; i<=n; i++) {
+        for(int j; j<=m; j++) {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    scanf("%d%d%c%d",&x,&y,&s,&k);
+    for(int i=0;i<=3;i++){  
+        if(dir[i]==s){
+            diri=i;
+            break;
+        }
+    }
+    int steps=0;
+    while(steps==k) {
+        if(a[x][y]==white) {
+            a[x][y]=black;
+            diri--;
+            if(dir[diri]=='R'){
+                y++;
+            }
+            if(dir[diri]=='L'){
+                y--;
+            }
+            if(dir[diri]=='U'){
+                x++;
+            }
+            if(dir[diri]='D'){
+                x--;
+            }
+        }
+        if(a[x][y]==black) {
+            a[x][y]=white;
+            diri++;
+            if (dir[diri] == 'R')
+            {
+                y++;
+            }
+            if (dir[diri] == 'L')
+            {
+                y--;
+            }
+            if (dir[diri] == 'U')
+            {
+                x++;
+            }
+            if (dir[diri] = 'D')
+            {
+                x--;
+            }
+        }
+    }
+    printf("%d %d",x,y);
+
+}
