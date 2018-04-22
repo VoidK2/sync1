71dfed0da60d16f6f4828bddea5aaf0968f934e6
voidk2 <1399477504@qq.com>
2018-04-22 13:59:54 +0800

    1

--- 0422A.cpp
+++ 0422A.cpp
@@ -6,10 +6,9 @@ int main() {
     double ans[5];
     memset(ans,0,sizeof(ans));
     while(scanf("%lf%lf%lf",&a,&b,&c)!=EOF){
-
         ans[0]=a-60;
         ans[1]=b-60;
-        ans[2]=180-a-b;
+        ans[2]=180-ans[0]-ans[1];
         sort(ans,ans+3);
         printf("%.4lf %.4lf %.4lf\n",ans[0],ans[1],ans[2]);
     }
