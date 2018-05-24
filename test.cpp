3da9db7a414bf4bca2b15d71859383973c3104bb
voidk2 <1399477504@qq.com>
2018-05-24 21:09:05 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 using namespace std;
 int main(){
-    double a=98.99;
-    //a*=100;
-    int b=(int)(a*100);
-    printf(" a=%d b=%d",(int)a,b);
+    int a=0;
+    a++;
+    a++;
+    a++;
+    a++;
+    printf("%d",a);
     return 0;
 }
