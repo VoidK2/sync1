72f4b39fda7e47d47876bec1336ad7fbecb760d4
voidk2 <1399477504@qq.com>
2018-07-11 21:25:44 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include <cstdio>
 using namespace std;
-int main(){
-  freopen("input.txt","r",stdin);
-  float a1,b1,c1,a2,b2,c2;
-  scanf("%f%f%f",&a1,&b1,&c1);
-  scanf("%f%f%f",&a2,&b2,&c2);
-
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  int a = 0;
+  scanf("%d", &a);
+  a++;
+  a++;
+  a++;
+  a = a++;
+  printf("%d", a);
+  return 0;
 }
